cc508a623f308b7bf3ae81a82280583267d3212b
Albin Strömberg <albin.stromberg@knowit.se>
2018-01-18 07:21:59 +0100

    initial commit

---
+++ guidgen.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    //För Kompliering
+    //%WINDIR%\Microsoft.NET\Framework\v2.0.50727\csc.exe guidgen.cs 
+
+    static void Main(string[] args)
+    {
+        if (args != null && args.Length > 0)
+        {
+            if (args[0] == "?" || args[0] == "-?" || args[0] == "/?")
+            {
+                System.Console.WriteLine();
+                System.Console.WriteLine("Usage:");
+                System.Console.WriteLine("\t> guid -[count(int)]");
+                System.Console.WriteLine();
+                System.Console.WriteLine("Examples:");
+                System.Console.WriteLine("\t> guid \t ...Gives one GUID");
+                System.Console.WriteLine("\t> guid -2 \t ...Gives two GUIDs and so on");
+            }
+            else
+            {
+                System.Console.WriteLine();
+                try
+                {
+                    string arg = args[0].TrimStart('/').TrimStart('-');
+                    int loop = System.Convert.ToInt32(arg);
+                    if (loop > 1)
+                    { 
+                        System.Console.WriteLine("New GUID's");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("New GUID");
+                    }
+                    System.Console.WriteLine("====================================");
+                    if (loop > 0)
+                    {
+                        for (int i = 0; i < loop; i++)
+                        {
+                            DoPrint();
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("New GUID");
+                    System.Console.WriteLine("====================================");
+                    DoPrint();
+                }
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("New GUID");
+            System.Console.WriteLine("====================================");
+            DoPrint();
+        }
+    }
+
+    static void DoPrint()
+    {
+        string guid = System.Guid.NewGuid().ToString();
+        System.Console.WriteLine(guid);
+    }
+}
