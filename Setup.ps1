8c5affdffcb9f5fe375c0996520379cbc514e974
Albin Strömberg <albin.stromberg@knowit.se>
2020-02-28 09:43:40 +0100

    Added setupscirpt and updated readme

---
+++ Setup.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = "Stop"
+Clear-Host
+try {
+    Write-Host "COMPILE" -ForegroundColor Magenta
+    Write-Host "-------------------------------------------------------------------------" -ForegroundColor Magenta
+	Write-Host "1.1 - Begin to complie" -ForegroundColor Magenta
+    $path = Resolve-Path guid.cs  
+    $compiler = "$env:windir/Microsoft.NET/Framework/v3.5/csc"  
+    &$compiler /target:exe guid.cs  
+    Write-Host "1.2 - Compliation complete" -ForegroundColor Magenta
+    Write-Host "-------------------------------------------------------------------------" -ForegroundColor Magenta
+} catch {
+    Write-Host ""
+    Write-Host "-------------------------------------------------------------------------" -ForegroundColor Red
+	Write-Host "ERROR: Failed compile file." -ForegroundColor Red
+	Write-Host $error[0]
+    Write-Host "-------------------------------------------------------------------------" -ForegroundColor Red
+	Exit 1
+}
+
+try {
+    Write-Host ""
+    Write-Host "COPY" -ForegroundColor Yellow
+    Write-Host "-------------------------------------------------------------------------"  -ForegroundColor Yellow
+	Write-Host "2.1 - Try to copy guid.exe to system32" -ForegroundColor Yellow
+    Copy-Item "guid.exe" -Destination "$env:windir/System32/"
+    Write-Host "2.2 - Copy completed, enjoy guid in the Windows Console." -ForegroundColor Yellow
+    Write-Host "-------------------------------------------------------------------------"  -ForegroundColor Yellow
+} catch {
+    Write-Host ""
+    Write-Host "-------------------------------------------------------------------------" -ForegroundColor Red
+	Write-Host "ERROR: Failed copy file." -ForegroundColor Red
+	Write-Host $error[0] -ForegroundColor Red
+    Write-Host "-------------------------------------------------------------------------" -ForegroundColor Red
+	Exit 1
+}
